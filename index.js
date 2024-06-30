f4a28feb8fc1b90628cc613b519d4ac495581fce
Rodgers Chesoni <rodgerschesoni@gmail.com>
2024-06-30 16:34:16 +0300

    add arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  });
+  
+  function destructivelyAppendCat(name) {
+    cats.push('Ralph');
+  }
+  function destructivelyPrependCat(name){
+    cats.unshift('Bob');
+  }
+  function destructivelyRemoveLastCat(){
+    cats.pop();
+  }
+  function destructivelyRemoveFirstCat(){
+    cats.shift();
+  }
+  function appendCat(name){
+   return [...cats, "Broom"];
+  }
+  function prependCat(name){
+    return ["Arnold", ...cats];
+  }
+  function removeLastCat(){
+   return cats.slice(0, -1);
+  }
+  function removeFirstCat(){
+    return cats.slice (1);
+  }
